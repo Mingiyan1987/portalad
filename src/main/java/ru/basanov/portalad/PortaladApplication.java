352688a426752f6bedaf4c37aea56fc4ede9f669
Mingiyan1987 <basanov.mt87@gmail.com>
2019-03-08 09:46:15 +0300

    1

---
+++ src/main/java/ru/basanov/portalad/PortaladApplication.java
@@ -0,0 +1,13 @@
+package ru.basanov.portalad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortaladApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortaladApplication.class, args);
+    }
+
+}
