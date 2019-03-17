7bf88a34a92afdd5f27be22aec88bfe7ac62029a
Mingiyan1987 <basanov.mt87@gmail.com>
2019-03-17 13:12:37 +0300

    1

--- src/main/java/ru/basanov/portalad/PortaladApplication.java
+++ src/main/java/ru/basanov/portalad/PortaladApplication.java
@@ -1,10 +1,13 @@
 package ru.basanov.portalad;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
 import ru.basanov.portalad.controller.UserController;
 
 
@@ -15,5 +18,4 @@ public class PortaladApplication {
     public static void main(String[] args) {
         SpringApplication.run(PortaladApplication.class, args);
     }
-
 }
