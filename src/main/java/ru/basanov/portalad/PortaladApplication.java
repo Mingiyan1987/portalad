6383f519233afc6e91fdbbd308127c85c4a1a315
Mingiyan1987 <basanov.mt87@gmail.com>
2019-03-26 08:01:04 +0300

    Добавил RSSReader, читает по ссылке без записи в БД

--- src/main/java/ru/basanov/portalad/PortaladApplication.java
+++ src/main/java/ru/basanov/portalad/PortaladApplication.java
@@ -13,6 +13,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.basanov.portalad.controller.UserController;
+import ru.basanov.portalad.dto.FeedMessage;
+import ru.basanov.portalad.model.Feed;
+import ru.basanov.portalad.parser.RSSFeedParser;
 
 import java.net.URL;
 
@@ -23,5 +26,13 @@ public class PortaladApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PortaladApplication.class, args);
+        RSSFeedParser parser = new RSSFeedParser(
+                "http://www.oracle.com/ocom/groups/public/@ocom/documents/webcontent/195094.xml");
+        Feed feed = parser.readFeed();
+        System.out.println(feed);
+        for (FeedMessage message : feed.getMessages()) {
+            System.out.println(message);
+
+        }
     }
 }
